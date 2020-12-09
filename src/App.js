1644ed36ea5503bf13a5b7be2c08e3f577ac3314
pruthvirajmv <74802690+pruthvirajmv@users.noreply.github.com>
2020-12-09 22:52:17 +0530

    fix: output for 2 & 3 also updated gui

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import "./styles.css";
 
 export default function App() {
   const [output, check] = useState("");
+  // const fisrtTwoPrime = [2, 3];
 
   function primeNumberCheck(input) {
     var inputNum = input.target.value;
     var out = "Invalid Input";
+    console.log(inputNum);
+    // if (inputNum >= 0 && inputNum <= 1) {
+    //   out = "Prime Number starts from 2";
+    //   return check(out);
+    // } else
+    if (inputNum > 1 && inputNum < 4) {
+      out = "Prime Number";
+      // console.log(out);
+      return check(out);
+    }
+
     for (var i = 2; i <= inputNum / 2; i++) {
       if (inputNum % i === 0) {
         out = "Not a Prime Number";
@@ -16,33 +28,37 @@ export default function App() {
         return check(out);
       }
     }
-
+    // console.log(out);
     return check(out);
   }
 
   return (
     <div className="App">
       <h1>prime number check</h1>
-      <h2>Enter number to check prime or not</h2>
+      <h3>Enter number to check prime or not</h3>
+
       <input
         style={{
           fontFamily: "inherit",
           border: "2px solid",
           borderRadius: "0.2rem",
-          fontSize: "large"
+          fontSize: "large",
+          maxWidth: "150px"
         }}
         onChange={primeNumberCheck}
       />
       <div
         style={{
           paddingTop: "1rem",
-          fontSize: "1.5rem",
+          fontSize: "1.8rem",
           fontWeight: "bolder",
+          letterSpacing: "0.1rem",
           color: "green"
         }}
       >
         {output}
       </div>
+      <p>Note: prime numbers are greater than 1</p>
       <h4> Hope you enjoyed this quick and simple web app </h4>
       <h5>
         hit
